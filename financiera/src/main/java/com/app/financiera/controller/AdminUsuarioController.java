28263e24de37d60c70d9d4edbd4ecf9b19c6ae8e
orlando572 <orlandooficial021@gmail.com>
2025-10-20 01:15:18 -0500

    Se hiciero cambios importantes, se agrego el gestion de pesiones y el controlador de adminUsuario, para separar la responsabilidad

---
+++ financiera/src/main/java/com/app/financiera/controller/AdminUsuarioController.java
@@ -0,0 +1,9 @@
+package com.app.financiera.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/usuarios")
+public class AdminUsuarioController {
+}
